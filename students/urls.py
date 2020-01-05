0d9069951d340e4e00c6963dad30470c1e1fe261
Pavlo Mykhalichenko <pavlom@oneplanetops.com>
2020-01-05 16:32:32 +0200

    HT-4 -- lesson

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [  
-    path('', views.get_student, name='get_student'),
+    path('student/', views.get_student, name='get_student'),
+    path('students/', views.get_students, name='get_students'),
 ]
--- students/views.py
+++ students/views.py
@@ -6,4 +6,31 @@ from .models import Student
 def get_student(request):
     Student.create_student()
     return render(request, 'student_data.html', {'student': Student.objects.last()})
+
+
+def get_students(request):
+    print('request.GET: ', request.GET)
+    queryset = Student.objects.all()
+    response = ''
+    fn = request.GET.get('first_name')
+    if fn:
+        # __contains LIKE %{}%
+        queryset = queryset.filter(first_name__contains=fn)
+        # __endswith LIKE %{}
+        # queryset = queryset.filter(first_name__endswith=fn)
+        # __startswith LIKE {}%
+        # queryset = queryset.filter(first_name__startswith=fn)
+        #Регистронезависимое
+        # __contains ILIKE %{}%
+        # queryset = queryset.filter(first_name__icontains=fn)
+        # __endswith ILIKE %{}
+        # queryset = queryset.filter(first_name__iendswith=fn)
+        # __startswith ILIKE {}%
+        # queryset = queryset.filter(first_name__istartswith=fn)
+    for student in queryset:
+        response += student.get_info()+'<br>'
+    print('queryset: ', queryset.query)
     
+    return render(request,
+                'students_list.html',
+                context={'students_list': response})
